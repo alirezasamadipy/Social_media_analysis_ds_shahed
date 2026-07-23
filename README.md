cc23a9f2d8b314ac98a1e90b1260c5b265c1f6b9
alirezasamadi <alireza.samadi.84.5.14@gmail.com>
2026-07-23 15:00:06 +0330

    Proccess interface,network data,distances

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # Social_media_using_graph_analysis
-Run the run.py for GUI test.
-main.py have the algorithms and input proccess and core of project functions.
+Run the run.py for GUI test.\n
+main.py have the algorithms and input proccess and core of project functions.\n
 
+main.py input:text file format -->(name_of_node1+" "+name_of_node2+" "+weight_of_edge)\n
+
+main.py timecomplexities:\n
+creating a object of Proccess(data)--> O(# lines in text file)\n
+Proccess(data).BFS(node("name of node"))-->O(V + E)\n
+Proccess(data).group() --> O(V + E)\n
+Proccess(data).find_friend(node("name of node")) --> O((# neighbors)*(# neighbors of neighbors))\n
+Proccess(data).popular_person() --> O(V)\n
+Proccess(data).intersecion(node("name of node") , node("name of node")) -- > O(min(len(s1) , len(s2)))\n
+Proccess(data).path(node("name of node") , node("name of node")) -- > O(V + E)\n
+Proccess(data).network() -- > O(# groups * (V + E))\n
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #algorithms  , input proccess , data will be here. 
 from collections import deque , defaultdict
-data =  open("data.txt")
+from math import inf
+data =  open("/media/alireza/Local Disk/ALIREZA/VS CODE/Social media project with Graph(data structure course)/data.txt")
 
-class node:
+class node: 
     def __init__(self, name):
         self.name = name
         self.parent = None
+        self.d = inf
 
     def __str__(self):
         return f"Node({self.name})"
@@ -16,20 +18,28 @@ class node:
     def __hash__(self):
         return hash(self.name)
 
-Graph = defaultdict(set)
-groups = []
+
+groups = []  #groups list declaration
 
 
 
-def txtproccess(g:defaultdict , txt):
+def txtproccess(txt):  #proccess the input text file
+    e = 0  #number of edges
+    g = defaultdict(set)  #graph network declaration
     for line in txt:
         k,v =line[:-2].split()
-        g[node(k)].add(node(v))
-        g[node(v)].add(node(k))
+        k = node(k)
+        v = node(v)
+        if v in g[k] :
+            e -= 1
+        g[k].add(v)
+        g[v].add(k)
+        e += 1
+    return (g , e)
     
 
 
-def shortest_path(g:defaultdict,a:node , b:node) -> list:
+def shortest_path(g:defaultdict,a:node , b:node) -> list: # finding the shortest path
     b = BFS(g , start=a , stop=b)
     lst = [b]
     #s=f"{b}"
@@ -46,6 +56,11 @@ def shortest_path(g:defaultdict,a:node , b:node) -> list:
 def BFS(g:defaultdict,start:node , stop = node("not set"),grouping = False):
     #print(start)
     #print(stop)
+    for u in g.keys():
+        u.d = inf
+    start.d = 0
+    dlist = defaultdict(set)
+    dlist[start.d] = start
     visited = set()
     que = deque()
     que.append(start)
@@ -58,6 +73,8 @@ def BFS(g:defaultdict,start:node , stop = node("not set"),grouping = False):
         # Process
         for neighbor in g[node]:
             if neighbor not in visited:
+                neighbor.d = node.d + 1
+                dlist[neighbor.d].add(neighbor)
                 neighbor.parent = node
                 que.append(neighbor)
                 visited.add(neighbor)
@@ -65,19 +82,75 @@ def BFS(g:defaultdict,start:node , stop = node("not set"),grouping = False):
                     gg.pop(neighbor)
                 if neighbor == stop:
                     return neighbor
-    
+    if not grouping:
+        return dlist
     if grouping and gg:
         start = next(iter(gg))
         groups.append(BFS(gg , start , grouping=True))
     return visited
-    
+
+
+
+
+
+class Proccess():  #interface of main.py
+    def __init__(self , input_txt):
+        self.gg = None
+        self.g , self.e = txtproccess(input_txt)
+    def group(self): #grouping the network
+        global groups
+        groups.clear()
+        groups.append(BFS(self.g , next(iter(self.g)) , grouping=True))
+        self.gg = groups
+        return groups
+    def find_friend(self , start:node): #suggest friend for start node
+        friends = set()
+        for neighbor in self.g[start]:
+            for friend in self.g[neighbor]:
+                friends.add(friend)
+        return friends
+    def popular_person(self): # person with largest community and friends
+        person = []
+        mx = 0
+        for p in self.g.keys():
+            ln = len(self.g[p])
+            if mx < ln:
+                person.append(p)
+                mx = len(self.g[p])
+            elif mx == ln:
+                person.append(p)
+        return person
+    def intersecion(self , u:node , v:node): # friends of tow person
+        return self.g[u].intersection(self.g[v])
+    def path(self , start:node , stop:node):
+        return shortest_path(self.g , start , stop) #shortest path from b to a
+    def network(self):  # network data
+        lnn = len(self.g.keys())
+        if self.gg == None:
+            self.group()
+        biggest_group = set()
+        mx = 0
+        for p in range(len(self.gg)):
+            ln = len(self.gg[p])
+            if mx < ln:
+                biggest_group = self.gg[p]
+                mx = ln
+        return ( lnn , self.e , lnn/self.e , biggest_group , self.popular_person() ) #8.a , 8.b , 8.c , 8.d , 8.e
+    def BFS(self , start:node): # distance and list nodes from start node
+        return BFS(self.g , start)
 
     
 
 
-txtproccess(Graph ,data)
-groups.append(BFS(Graph , node("adlfj") , grouping=True))
-print( len(groups))
-#shortest_path(Graph , node("321") , node("adlfj"))
+
+            
 
 
+social = Proccess(data)
+print("___________BFS__________", social.BFS(node("F3")))
+print("___________group__________", social.group())
+print("___________find_friend__________", social.find_friend(node("F3")))
+print("___________popular_person__________", social.popular_person())
+print("___________intersecion__________", social.intersecion(node("G2") , node("F3")))
+print("___________path__________", social.path(node("G2") , node("F3")))
+print("___________network__________", social.network())
